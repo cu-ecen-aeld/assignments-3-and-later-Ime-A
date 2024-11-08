6301c12274eeef1e5338aa520a23a526184ca620
Ime <imeasamudo@gmail.com>
2024-11-08 09:13:25 -0700

    Added a working server

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -1,112 +1,113 @@
 #include <stdio.h>
 #include <sys/socket.h>
-#include <sys/socket.h>
 #include <netdb.h>
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <syslog.h>
 #include <fcntl.h>
-#include <syslog.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <unistd.h>
 
 #define BACKLOG 10
-#define MYPORT 9000
+#define MYPORT "9000"
 #define BUFFER_LENGTH 1024
-/*
-Create a socket based program with name aesdsocket in the “server” directory which:
 
-     a. Is compiled by the “all” and “default” target of a Makefile in the “server” directory and supports cross compilation, placing the executable file in the “server” directory and named aesdsocket.
+bool caught_sigint = false;
+bool caught_sigterm = false;
 
-     b. Opens a stream socket bound to port 9000, failing and returning -1 if any of the socket connection steps fail.
-
-     c. Listens for and accepts a connection
-
-     d. Logs message to the syslog “Accepted connection from xxx” where XXXX is the IP address of the connected client. 
-*/
-
-int main(int argc, char *argv[])
+void signal_handler(int signal_number)
 {
-    int sockfd = 0;
-    int status = 0;
-    int con_sockfd = 0;
+    if (signal_number == SIGINT || signal_number == SIGTERM)
+    {
+        syslog(LOG_INFO, "Caught signal, exiting");
+        caught_sigint = (signal_number == SIGINT);
+        caught_sigterm = (signal_number == SIGTERM);
+    }
+}
+
+int main(int argc, char** argv)
+{
+
+    if(argc )
+    openlog("aesdsocket", LOG_PID, LOG_USER);
+
+    struct sigaction new_action = {0};
+    new_action.sa_handler = signal_handler;
+    sigaction(SIGTERM, &new_action, NULL);
+    sigaction(SIGINT, &new_action, NULL);
+
+    int sockfd, con_sockfd;
     char client_ip[BUFFER_LENGTH];
-
-    struct addrinfo hints;
-    struct addrinfo* res = NULL;
+    char buffer[BUFFER_LENGTH];
+    struct addrinfo hints = {0}, *res;
     struct sockaddr_storage client_addr;
-    socklen_t addr_size;
+    socklen_t addr_size = sizeof(client_addr);
 
-    memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_flags = AI_PASSIVE;
 
-    status = getaddrinfo(NULL, "9000", &hints, &res);
-
-    if(status != 0)
-    {
-        perror("Failed to get addres.");
+    if (getaddrinfo(NULL, MYPORT, &hints, &res) != 0) {
+        syslog(LOG_ERR, "Failed to get address info.");
         return -1;
     }
 
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    if(sockfd < 0)
-    {
-        perror("Failed to obtain a socket file descriptor.");
-        close(sockfd);
+    if (sockfd < 0) {
+        syslog(LOG_ERR, "Socket creation failed.");
+        freeaddrinfo(res);
         return -1;
     }
 
-    status = bind(sockfd, res->ai_addr, res->ai_addrlen);
-    if(status != 0)
-    {
-        perror("Failed to get bind the socket.");
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0) {
+        syslog(LOG_ERR, "Socket bind failed.");
         close(sockfd);
+        freeaddrinfo(res);
         return -1;
     }
 
-    status = listen(sockfd, BACKLOG);
-    if(status != 0)
-    {
-        perror("Failed to get find a client.");
-        close(sockfd);
-        return -1;
-    }
+    freeaddrinfo(res);
 
-    printf("Listening on port 9000...\n");
+    while (!caught_sigint && !caught_sigterm) {
+        listen(sockfd, BACKLOG);
+        syslog(LOG_INFO, "Listening on port %s...", MYPORT);
 
-    con_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
-    if(con_sockfd < -1)
-    {
-        perror("Failed to get find a client.");
-        close(sockfd);
-        return -1;
-    }
+        con_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &addr_size);
+        if (con_sockfd < 0) continue;
 
+        if (getnameinfo((struct sockaddr *)&client_addr, addr_size, client_ip, sizeof(client_ip), NULL, 0, NI_NUMERICHOST) == 0)
+            syslog(LOG_INFO, "Accepted connection from %s", client_ip);
 
-    status = getnameinfo( (struct sockaddr *)&client_addr, addr_size, client_ip, sizeof(client_ip), NULL, 0, NI_NUMERICHOST);
-    if (status != 0)
-    {
-        perror("Failed to get IP address.");
-        close(sockfd);
+        int fd = open("/var/tmp/aesdsocketdata", O_WRONLY | O_APPEND | O_CREAT, 0644);
+        if (fd == -1) {
+            syslog(LOG_ERR, "File open failed.");
+            close(con_sockfd);
+            continue;
+        }
+
+        ssize_t bytes_received;
+        while ((bytes_received = recv(con_sockfd, buffer, BUFFER_LENGTH, 0)) > 0) {
+            char *newline_pos = strchr(buffer, '\n');
+            ssize_t write_size = newline_pos ? newline_pos - buffer + 1 : bytes_received;
+            write(fd, buffer, write_size);
+            if (newline_pos) break;
+        }
+        close(fd);
+
+        fd = open("/var/tmp/aesdsocketdata", O_RDONLY);
+        ssize_t bytes_read;
+        while ((bytes_read = read(fd, buffer, BUFFER_LENGTH)) > 0)
+            send(con_sockfd, buffer, bytes_read, 0);
+
+        close(fd);
         close(con_sockfd);
-        return -1;
+        syslog(LOG_INFO, "Closed connection from %s", client_ip);
     }
-
-    syslog(LOG_INFO, "Accepted connection from %s",client_ip);
-
-    
-
-    int fd = 0;
-
-    fd = open("/var/tmp/aesdsocketdata", O_WRONLY | O_APPEND | O_CREAT, 0644);
-    if (fd == -1) {
-        perror("Failed to open or create /var/tmp/aesdsocketdata");
-        return;
-    }
-
 
     close(sockfd);
-    close(con_sockfd);
+    remove("/var/tmp/aesdsocketdata");
+    closelog();
     return 0;
 }
